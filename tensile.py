070a29a1097679c40a3dbe69ce39e67e280078a6
lukaszrychlowski <rychlowski@agh.edu.pl>
2023-02-17 16:58:04 +0100

    fix calc_total_elongation

--- tensile.py
+++ tensile.py
@@ -38,7 +38,7 @@ def calc_strain(extension, length):
     return strain
 
 def calc_total_elongation(df):
-    return df['strain'][-1]
+    return df['strain'].iloc[-1]
 
 def calc_slope(df, strain_min_limit, strain_max_limit):
     df['linear_range'] = (df['strain1'] >= strain_min_limit) & (df['strain1'] <= strain_max_limit)                  # Separate linear region based on strain_min and strain_max limits
@@ -65,25 +65,32 @@ for file in os.listdir(path):
     if file.endswith('.csv'):
         df = importer(path)
         sample_params = get_sample_params(path)
-        #if '02.02' in sample_params['label']:
-        df['stress'] = calc_stress(df['load'], sample_params['area'])
-        df['strain'] = calc_strain(df['extension'], sample_params['length'])
-        df['true stress'] = calc_true_stress(df['stress'], df['strain'])
-        df['true strain'] = calc_true_strain(df['strain'])
-        df = calc_slope(df, strain_min_limit=0.001, strain_max_limit=0.002)
-        idx = calc_yield(df['stress_linear_offset'],df['stress'])
-        df['label'] = sample_params['label']
-        df['path'] = path+file
-        print(sample_params['label'] + ';' + str(np.round(df['stress'][idx].min(), 2)) + ';' + str(np.round(df['strain'].iloc[-1]*100, 2)))
-        axs[0].scatter(df['strain1'], df["stress"], s=0.5, label=sample_params['label']+ ' ' + file)
-        axs[0].plot(df['strain1'], df["stress_linear_offset"], linewidth=0.2)
-        axs[0].scatter(df['strain1'][idx], df['stress'][idx], s=15, marker='x', c='red')
-        axs[1].scatter(df['strain'], df['stress'], s=0.2,label=sample_params['label']+ ' ' + file)
-        axs[0].set_xlim(0, 0.06)
-        axs[0].set_ylim(0, 550)
-        axs[1].set_xlim(0, 0.2)
-        axs[1].set_ylim(0, 550)
-
-mplcursors.cursor(highlight=True).connect("add", lambda sel: sel.annotation.set_text(sel.artist.get_label()))   
+        if '13.01_seg9' in sample_params['label']:
+            df['stress'] = calc_stress(df['load'], sample_params['area'])
+            df['strain'] = calc_strain(df['extension'], sample_params['length'])
+            df['true stress'] = calc_true_stress(df['stress'], df['strain'])
+            df['true strain'] = calc_true_strain(df['strain'])
+            df = calc_slope(df, strain_min_limit=0.001, strain_max_limit=0.002)
+            idx = calc_yield(df['stress_linear_offset'],df['stress'])
+            df['label'] = sample_params['label']
+            df['path'] = path+file
+            print(sample_params['label'] + ';' + str(np.round(df['stress'][idx].min(), 2)) + ';' + str(np.round(df['strain'].iloc[-1]*100, 2)))     #label;yield;total_elongation
+            axs[0].scatter(df['strain1'], df["stress"], s=1, label=sample_params['label']+ ' ' + file)
+            axs[0].plot(df['strain1'], df["stress_linear_offset"], linewidth=0.5)
+            axs[0].scatter(df['strain1'][idx], df['stress'][idx], s=15, marker='x', c='red')
+            axs[1].scatter(df['strain'], df['stress'], s=1,label=sample_params['label']+ ' ' + file)
+            # axs[0].set_xlim(0, 0.06)
+            # axs[0].set_ylim(0, 550)
+            # axs[1].set_xlim(0, 0.2)
+            # axs[1].set_ylim(0, 550)
+           
+mplcursors.cursor(highlight=True).connect("add", lambda sel: sel.annotation.set_text(sel.artist.get_label()))
+axs[0].grid(which='both', color='gray', linewidth=0.5, alpha=0.5)
+axs[1].grid(which='both', color='gray', linewidth=0.5, alpha=0.5)
+axs[0].minorticks_on()
+axs[1].minorticks_on()
+#plt.grid(color='gray', linestyle='-', linewidth=0.5)
+plt.legend(fontsize='small')
+plt.savefig(path+file+'_.png', dpi=300)
 plt.show()
 
